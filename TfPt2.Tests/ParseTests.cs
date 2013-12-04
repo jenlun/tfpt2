21f602fbca54f5d30b2bae227ca5f73dfa80e19b
Jens Lundberg <jenslundberg72@gmail.com>
2013-12-04 21:09:25 +0800

    Update tests

--- TfPt2.Tests/ParseTests.cs
+++ TfPt2.Tests/ParseTests.cs
@@ -11,10 +11,10 @@
     {
         private const string History = @"Changeset Change                     User              Date       Comment
 --------- -------------------------- ----------------- ---------- -----------------------------------------------------------------------------------
-28        edit                       BETSSON\mica1     10/11/2013 SBI ? - Updated for sonar coding standard compliance
-21868     merge                      BETSSON\mica1     6/6/2013   Merge for HyperMarket wk 21 GameResult Bug Fix to Prod
-21579     merge, edit                BETSSON\mica1     5/29/2013  Merge for week 20 Hypermarket QA deploy
-19493     merge, branch              BETSSON\joil      3/18/2013  SBI-179 Sporting Solution Game Action/ Result Refactoring";
+28        edit                       XXXXXXX\mica1     10/11/2013 SBI ? - Updated for sonar coding standard compliance
+21868     merge                      XXXXXXX\mica1     6/6/2013   Merge for HyperMarket wk 21 GameResult Bug Fix to Prod
+21579     merge, edit                XXXXXXX\mica1     5/29/2013  Merge for week 20 Hypermarket QA deploy
+19493     merge, branch              XXXXXXX\joil      3/18/2013  SBI-179 Sporting Solution Game Action/ Result Refactoring";
 
         private static readonly string AnnotatedLine = @"28           public class BasketballGameActionHandler : IGameActionHandler";
 
@@ -25,11 +25,11 @@
             changeSets.AddHistory(History);
             //Assert.AreEqual(4, changeSets.Count);
 
-            Assert.AreEqual(@"28, BETSSON\mica1, 10/11/2013", changeSets.Get("28"));
+            Assert.AreEqual(@"28, XXXXXXX\mica1, 10/11/2013", changeSets.Get("28"));
 
             var transform = new TransformAnnotation(changeSets, AnnotatedLine);
             var transformed = transform.GetTransformedAnnotation();
-            Assert.AreEqual(@"28, BETSSON\mica1, 10/11/2013           public class BasketballGameActionHandler : IGameActionHandler" + Environment.NewLine, transformed);
+            Assert.AreEqual(@"28, XXXXXXX\mica1, 10/11/2013           public class BasketballGameActionHandler : IGameActionHandler" + Environment.NewLine, transformed);
         }
     }
 }
